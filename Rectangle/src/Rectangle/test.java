e86db87b88b851a4713d4523fb6ef7ebdae8e4fb
Saksham Sethi <saksham.sethi.123.123@gmail.com>
2024-08-28 11:14:38 -0400

    Add ./Rectangle/src/Rectangle/test.java

---
+++ Rectangle/src/Rectangle/test.java
@@ -0,0 +1,14 @@
+package Rectangle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
