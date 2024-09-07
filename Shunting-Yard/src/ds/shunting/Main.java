008563c0e3abc31b4f3cfa448c7c6406c525b6a7
Saksham Sethi <saksham.sethi.123.123@gmail.com>
2024-09-07 18:44:39 -0400

    Add ./Shunting-Yard/src/ds/shunting/Main.java

---
+++ Shunting-Yard/src/ds/shunting/Main.java
@@ -0,0 +1,10 @@
+package ds.shunting;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		InFixExpression inFix = new InFixExpression("3 + 2");
+		System.out.println(inFix);
+	}
+
+}
